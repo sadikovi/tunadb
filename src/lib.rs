3ec6f29c2b6c4270ba4d3542782000b106907331
Ivan Sadikov <ivan.sadikov@gmail.com>
2021-01-26 21:43:53 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// open a database or create a new one
+// - if directory does not exist -> new database, try to create it
+// - if directory exists but does not have a marker file -> not a database directory, cannot open
+// - if directory exists, has a marker file and no lock file -> existing database, open it
+// - if directory exists, has a marker file, and has a lock file -> existing database, cannot open
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn it_works() {
+    assert_eq!(2 + 2, 4);
+  }
+}
