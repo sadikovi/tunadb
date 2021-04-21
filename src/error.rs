b40972e926c9d3ff2bf62e5fa2d52bd0a1304ed8
Ivan Sadikov <ivan.sadikov@gmail.com>
2021-04-21 21:47:07 +0200

    update

---
+++ src/error.rs
@@ -0,0 +1,42 @@
+//! Module for various errors used in the project
+
+/// Generic error for the crate.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Error {
+  msg: String
+}
+
+impl Error {
+  pub fn msg(&self) -> &str {
+    self.msg.as_ref()
+  }
+}
+
+impl From<String> for Error {
+  fn from(msg: String) -> Self {
+    Self { msg: msg }
+  }
+}
+
+impl From<&str> for Error {
+  fn from(msg: &str) -> Self {
+    Self { msg: msg.to_owned() }
+  }
+}
+
+impl From<std::io::Error> for Error {
+  fn from(err: std::io::Error) -> Self {
+    Self { msg: format!("IO Error: {}", err.to_string()) }
+  }
+}
+
+macro_rules! err {
+  ($fmt:expr) => (crate::error::Error::from($fmt.to_owned()));
+  ($fmt:expr, $($args:expr),*) => (crate::error::Error::from(format!($fmt, $($args),*)));
+  ($e:expr, $fmt:expr) => (crate::error::Error::from($fmt.to_owned(), $e));
+  ($e:ident, $fmt:expr, $($args:tt),*) =>
+    (crate::error::Error::from(&format!($fmt, $($args),*), $e));
+}
+
+/// Internal type for the Result.
+pub type Res<T> = Result<T, Error>;
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,5 @@
-// open a database or create a new one
-// - if directory does not exist -> new database, try to create it
-// - if directory exists but does not have a marker file -> not a database directory, cannot open
-// - if directory exists, has a marker file and no lock file -> existing database, open it
-// - if directory exists, has a marker file, and has a lock file -> existing database, cannot open
+//! Test database.
 
-// mod btree;
-// mod page;
-
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
-}
+#[macro_use]
+pub mod error;
+pub mod page;
